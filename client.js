ff1fd2fa8956b3030ab7883d5cdb90fd7cf63ea2
Matt Dow <matt.dow.vo@gmail.com>
2021-09-28 15:04:23 -0500

    Completed base and unformatted stretch goals

--- client.js
+++ client.js
@@ -39,4 +39,73 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+$(document).ready(readyNow);
+
+function calculateBonusPercentage(employee) {
+  let result = 0;
+
+  switch (employee.reviewRating) {
+    case 3:
+      result += 4;
+      break;
+    case 4:
+      result += 6;
+      break;
+    case 5:
+      result += 10;
+      break;
+    default:
+      result = 0;
+  }
+  if (employee.employeeNumber.length === 4) {
+    result += 5;
+  }
+  if (Number(employee.annualSalary) > 65000) {
+    result -= 1;
+  }
+
+  if (result > 13) {
+    result = 13;
+  } else if (result < 0) {
+    result = 0;
+  }
+  return result;
+}
+
+console.log(employees);
+console.log(calculateBonusPercentage(employees[0]));
+console.log(calculateBonusPercentage(employees[1]));
+console.log(calculateBonusPercentage(employees[2]));
+console.log(calculateBonusPercentage(employees[3]));
+console.log(calculateBonusPercentage(employees[4]));
+
+function bonusCalculator(employee) {
+  let result = {};
+  result.name = employee.name;
+  result.bonusPercentage = calculateBonusPercentage(employee);
+  let totalComp = (Number(employee.annualSalary)) * (1 + result.bonusPercentage / 100);
+  totalComp = (Math.round(totalComp * 10)) / 10;
+  // console.log(totalComp);
+  result.totalCompensation = Math.round(totalComp);
+  result.totalBonus = result.totalCompensation - Number(employee.annualSalary);
+  return result;
+}
+
+
+function readyNow() {
+  console.log('JQ');
+  $("#btn").on('click', calcAllBonuses);
+}
+
+function calcAllBonuses() {
+  
+  for (let employee of employees) {
+    let el = bonusCalculator(employee);
+    $("#list").append(`<li>${el.name} - Bonus Percentage: ${el.bonusPercentage}, Total Comp: ${el.totalCompensation}, Total Bonus: ${el.totalBonus}</li>`);
+
+  }
+}
+for (let employee of employees) {
+  console.log(bonusCalculator(employee));
+}
+
